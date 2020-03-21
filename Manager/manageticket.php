997298685ce116d6658828bba637a8552e2072b6
Toufique-E-Elahi <teeleven011@gmail.com>
2020-03-21 21:48:50 +0600

    Manage Ticket & Transport Design

--- Manager/manageticket.php
+++ Manager/manageticket.php
@@ -1,52 +1,61 @@
 <html>
     <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="css/home.css">
+        <link rel="stylesheet" type="text/css" href="css/manageticket.css">
     </head>
-    <body style="background-color: lightgray;">
+    <body>
        
            
-        <div class="title" >TOURISM MANAGEMENT SYSTEM</div>
+        <div class="title" >TOURISM MANAGEMENT SYSTEM
+        </div>
+         
         <div class="dropdown">
             <button class="dropbtn"><i class="fa fa-bars">&nbsp;&nbsp;&nbsp;Menu</i></button>
                 <div class="dropdown-content">
-                    <!-- These Links will be changed later -->
                     <button class="btn" onClick="location.href='home.php'" value='home'><i class="fa fa-home">&nbsp;&nbsp;&nbsp;Home</i></button><br>
                     <button class="btn" onClick="location.href='manageticket.php'" value='manageticket'><i class="fa fa-plane">&nbsp;&nbsp;&nbsp;Manage Ticket</i></button><br>
                     <button class="btn" onClick="location.href='createtransport.php'" value='createtransport'><i class="fa fa-plane">&nbsp;&nbsp;&nbsp;Create Transport</i></button><br>
                     <button class="btn" onClick="location.href='managetransport.php'" value='managetransport'><i class="fa fa-plane">&nbsp;&nbsp;&nbsp;Manage Transport</i></button><br>
                     <button class="btn" onClick="location.href='createhotel.php'" value='createhotel'><i class="fa fa-plane">&nbsp;&nbsp;&nbsp;Create Hotel</i></button><br>
                     <button class="btn" onClick="location.href='managehotel.php'" value='managehotel'><i class="fa fa-plane">&nbsp;&nbsp;&nbsp;Manage Hotel</i></button><br>
-                    <button class="btn" onClick="location.href='webinfoediting.php'" value='webinfoediting' ><i class="fa fa-user-circle">&nbsp;&nbsp;&nbsp;Website Info Editing</i></button><br>
+                    <button class="btn" onClick="location.href='webinfoediting.php'" value='webinfoediting'><i class="fa fa-user-circle">&nbsp;&nbsp;&nbsp;Website Info Editing</i></button><br>
                     <button class="btn" onClick="location.href='manageissue.php'" value='manageissue'><i class="fa fa-comments">&nbsp;&nbsp;&nbsp;Manage Issues</i></button><br>
                     <button class="btn" onClick="location.href='managebill.php'" value='managebill'><i class="fa fa-comments">&nbsp;&nbsp;&nbsp;Manage Bills</i></button><br>
                     <button class="btn" onClick="location.href='editprofile.php'" value='editprofile'><i class="fa fa-user">&nbsp;&nbsp;&nbsp;Edit Profile</i></button><br>
                     <button class="btn" onClick="location.href='changepassword.php'" value='changepassword'><i class="fa fa-key">&nbsp;&nbsp;&nbsp;Change Password</i></button><br>
-                    <!-- These Links will be changed later -->
                     <button class="btn"><i class="fa fa-sign-out">&nbsp;&nbsp;&nbsp;Log Out</i></button><br>
                 </div>
                 
         </div>
 
         <div class="welcome" ><i class="fa fa-user">&nbsp;&nbsp;&nbsp;WELCOME XYZ</i>
+
         </div>
-
-       
-
-        <div class="text" >Home</i>
+        <div class="text" >Manage Ticket
+        
         </div>
-
 
         <div class="panel">
-            <input type="button" value="users 17" style="background-color:green"> 
-            <input type="button" value="reviews 20" style="background-color:rgb(153, 102, 255);margin-left: 60px">
-            <input type="button" value="bookings 170" style="background-color:rgb(255, 153, 0);margin-left: 60px"> <br>
+        
+            <table>
+  <tr>
+  <th>TICKET ID</th>
+  <th>PRICE</th>
+  <th>BOOKING STATUS</th>
+  <th>REFERENCE</th>
+  
+  
+  </tr>
+  <tr>
+  <td>T-1</td>
+  <td>$500</td>
+  <td>BOOKED</td>
+  <td>RFE-15</td>
+</tr>
 
-            <input type="button" value="issues 17" style="background-color:rgb(255, 51, 153)"> 
-            <input type="button" value="tickets 20" style="background-color:rgb(0, 230, 230);margin-left: 60px">
-            <input type="button" value="booking issues 170" style="background-color:rgb(179, 0, 179);margin-left: 60px"> 
-
-        </div>
+  
+        </table>
+    </div>
         
     </body>
 </html>
