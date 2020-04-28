d31627b90daf20ffd7932776e398683b444ecab7
Mustak-Khaled <mustak.khaled@gmail.com>
2020-04-28 15:05:26 +0600

    create_about_storage_1st

---
+++ final_project/views/User/about.php
@@ -0,0 +1,49 @@
+<html>
+    <head>
+        <title>About</title>
+        <link rel="stylesheet" type="text/css" href="Css/home.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+        <style>
+            .mySlides {display:none;}
+            body, html {
+            height: 100%;
+            margin: 0;
+            }
+            .bg {
+            background-image: url("../../storage/package_image/about.jpg");
+            height: 100%; 
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            }
+        </style>
+    </head>
+    <body class="bg">
+        <ul class="active">
+            <li><a href="home.php">Home</a></li>
+            <li><a href="packages.php">Packages</a></li>
+            <li><a href="hotel.php">Hotels</a></li>
+            <li><a href="gallery.php">Gallery</a></li>
+            <li><a href="Transport.php">Transprot</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="#contact"><input type="text"><input type="button" value="search"></a></li>
+            <li><a href="profile.php">Login/Signup</a></li>
+          </ul>
+
+        <div style="position:absolute; top: 100px; left: 30px;">
+            <font size="60"><h1>About Us</h1><hr> </font>
+        </div>
+        <div style="position:absolute; top: 240px; left: 30px;">
+            <font><h1 style="font-size: x-large; color: azure;">We are passionate about travel and sharing the world's wonders
+                 on the leisure travel side, and 
+                 <br>providing corporate 
+                 travelers hi-touch services to 
+                 facilitate their business travel needs. We're an
+                 <br>employee-owned travel agency anchored 
+                 by our values, integrity and dedication to customer service.</h1> </font>
+        </div>
+        
+    </body>
+</html>
